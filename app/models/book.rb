ba8d09946b2038d61a3b2feeaa146ff01d1bbc98
ahotchkin <allyson.hotchkin@gmail.com>
2021-01-27 15:20:32 -0500

    Write out instance methods, add comments for validations and class method

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,4 +4,36 @@ class Book < ApplicationRecord
   has_many :book_reviews
   has_many :book_formats
   has_many :book_format_types, through: :book_formats
+
+  # title, publisher_id, author_id are all required
+
+  # remamed to book_format_type as calling this book_format_types produces an error since that is an attribute of self
+  def book_format_type
+    # Returns a collection of the BookFormatTypes this book is available in
+    self.book_format_types
+  end
+
+  def author_name
+    # The name of the author of this book in “lastname, firstname” format
+    "#{self.author.last_name}, #{self.author.first_name}"
+  end
+
+  def average_rating
+    # The average (mean) of all the book reviews for this book. Rounded to one decimal place.
+    ratings_array = self.book_reviews.collect { |book_review| book_review.rating }
+    (ratings_array.sum.to_f / ratings_array.length.to_f).round(1)
+  end
+
+  def self.search(query, options)
+    # Returns a collection of books that match the query string, subject to the following rules:
+      # 1. If the last name of the author matches the query string exactly (case insensitive)
+      # 2. If the name of the publisher matches the query string exactly (case insensitive)
+      # 3. If any portion of the book’s title matches the query string (case insensitive)
+    # The results should be the union of books that match any of these three rules. The results should be ordered by average rating, with the highest rating first. The list should be unique (the same book shouldn't appear multiple times in the results).
+    # Search options:
+      # :title_only (defaults to false). If true, only return results from rule #3 above.
+      # :book_format_type_id (defaults to nil). If supplied, only return books that are available in a format that matches the supplied type id.
+      # :book_format_physical (defaults to nil). If supplied as true or false, only return books that are available in a format whose “physical” field matches the supplied argument. This filter is not applied if the argument is not present or nil.
+    # The title_only and book_format options are not exclusive of each other, so Book.search('Karamazov', title_only: true, book_format_physical: true) should return all physical books whose title matches that term.
+  end
 end
